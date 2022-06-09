3ff45c24ad0f1bf8489d3a1596b75f816ad4447b
Ty <dutchessupperman@gmail.com>
2022-06-09 14:23:21 -0500

    .

---
+++ loginpage/function.js
@@ -0,0 +1,26 @@
+function loginOnClick()
+{
+    var user = document.getElementById("email").value;
+    
+    var password = document.getElementById("password").value;
+    var process = true;
+    if (user.length < 1)
+    {
+        alert("username is required");
+        process = false;
+    }
+    
+    if (password.length < 1)
+    {
+        alert("password is required");
+        process = false;
+    }
+    
+    if (process)
+    {
+        alert (user);
+        alert (password);
+    }    
+console.log("HERE");
+    return false;
+}
